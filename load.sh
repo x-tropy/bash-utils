d4023ddb2eab7e28a8b2c837e967403b09e62474
lavabyte <buweiliao@gmail.com>
2024-02-29 17:00:00 +0100

    ✨ Utility functions for Bash

---
+++ load.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+bash_utility_folder="/Users/haku/Developer/[5] System/[0] Bash/bash-utility"
+
+source "$bash_utility_folder/src/array.sh"
+source "$bash_utility_folder/src/string.sh"
+source "$bash_utility_folder/src/variable.sh"
+source "$bash_utility_folder/src/file.sh"
+source "$bash_utility_folder/src/misc.sh"
+source "$bash_utility_folder/src/date.sh"
+source "$bash_utility_folder/src/interaction.sh"
+source "$bash_utility_folder/src/check.sh"
+source "$bash_utility_folder/src/format.sh"
+source "$bash_utility_folder/src/collection.sh"
+source "$bash_utility_folder/src/json.sh"
+source "$bash_utility_folder/src/terminal.sh"
+source "$bash_utility_folder/src/validation.sh"
+source "$bash_utility_folder/src/debug.sh"
+source "$bash_utility_folder/src/os.sh"
